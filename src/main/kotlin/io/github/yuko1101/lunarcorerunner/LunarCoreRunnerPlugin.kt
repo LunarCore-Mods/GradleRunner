0c4380d407f8a00e704af5412babcbea7481f1c4
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2024-04-26 14:30:41 +0900

    created a custom task for runServer

--- src/main/kotlin/io/github/yuko1101/lunarcorerunner/LunarCoreRunnerPlugin.kt
+++ src/main/kotlin/io/github/yuko1101/lunarcorerunner/LunarCoreRunnerPlugin.kt
@@ -2,25 +2,32 @@ package io.github.yuko1101.lunarcorerunner
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.JavaExec
+import org.gradle.api.artifacts.Configuration
 import java.io.File
-import java.nio.file.Paths
 import java.util.jar.JarInputStream
 import java.util.jar.Manifest
 
 abstract class LunarCoreRunnerPlugin : Plugin<Project> {
+
+    companion object {
+        lateinit var lunarcore: Configuration
+        lateinit var gameprovider: Configuration
+        lateinit var gameproviderLibrary: Configuration
+
+    }
+
     override fun apply(project: Project) {
         val implementation = project.configurations.getByName("implementation")
         implementation.isCanBeResolved = true
         
-        val lunarcore = project.configurations.create("lunarcore") {
+        lunarcore = project.configurations.create("lunarcore") {
             implementation.extendsFrom(it)
         }
-        val gameprovider = project.configurations.create("gameprovider") {
+        gameprovider = project.configurations.create("gameprovider") {
             implementation.extendsFrom(it)
         }
         // does not extend implementation not to add any dependencies to the mod
-        val gameproviderLibrary = project.configurations.create("gameproviderLibrary")
+        gameproviderLibrary = project.configurations.create("gameproviderLibrary")
 
         // TODO: better way to get version
         val defaultMixinVersion = "0.12.5+mixin.0.8.5"
@@ -40,53 +47,7 @@ abstract class LunarCoreRunnerPlugin : Plugin<Project> {
         gameproviderLibrary.exclude(mapOf("module" to "gson"))
         gameproviderLibrary.exclude(mapOf("module" to "guava"))
 
-        project.tasks.register("runServer", JavaExec::class.java) { task ->
-            task.dependsOn("jar")
-
-            val lunarCoreFile: File = lunarcore.files.first()
-            val gameProvider: File = gameprovider.singleFile
-            val gameProviderLibraries: List<File> = gameproviderLibrary.toList()
-
-            val runDir = project.file("run")
-            val librariesDir = File(runDir, "libraries")
-            if (!librariesDir.exists()) librariesDir.mkdirs()
-            val modsDir = File(runDir, "mods")
-            if (!modsDir.exists()) modsDir.mkdir()
-
-            task.doFirst { _ ->
-                val modFile: File = project.tasks.getByName("jar").outputs.files.singleFile
-                modFile.copyTo(File(modsDir, modFile.name), true)
-
-            }
-
-            val lunarCoreJar = File(runDir, lunarCoreFile.name)
-            val gameProviderJar = File(runDir, gameProvider.name)
-            lunarCoreFile.copyTo(lunarCoreJar, true)
-            gameProvider.copyTo(gameProviderJar, true)
-
-            val libraryJars: List<File> = gameProviderLibraries.map { library ->
-                val to = File(librariesDir, library.name)
-                library.copyTo(to, true)
-                to
-            }
-
-            task.workingDir = runDir
-            task.classpath = project.files(lunarCoreJar, gameProviderJar, *libraryJars.toTypedArray())
-
-            val mainClass: String = gameProviderJar.inputStream().use { stream ->
-                val manifest: Manifest? = JarInputStream(stream).use { it.manifest }
-                manifest?.mainAttributes?.getValue("Main-Class")
-            } ?: throw IllegalStateException("Main-Class not found in the manifest of the mod jar")
-            task.mainClass.set(mainClass)
-
-            // loads mods from classpath
-            task.jvmArgs("-Dfabric.development=true")
-
-            val cp = task.classpath.joinToString(";") { runDir.toPath().relativize(it.toPath()).toString() }
-            File(runDir, "args.txt").writeText("-cp $cp\n${mainClass}")
-
-            task.setStandardInput(System.`in`)
-            task.setIgnoreExitValue(true)
+        project.tasks.register("runServer", RunServerTask::class.java) { task ->
         }
     }
 }
---
+++ src/main/kotlin/io/github/yuko1101/lunarcorerunner/RunServerTask.kt
@@ -0,0 +1,80 @@
+package io.github.yuko1101.lunarcorerunner
+
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.JavaExec
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskAction
+import org.gradle.work.DisableCachingByDefault
+import java.io.File
+import java.util.jar.JarInputStream
+import java.util.jar.Manifest
+
+@DisableCachingByDefault
+abstract class RunServerTask : JavaExec() {
+    @OutputDirectory
+    val runDir: Property<File> = this.objectFactory.property(File::class.java).apply { set(project.file("run")) }
+
+    init {
+        this.dependsOn("jar")
+
+        val cachedGameProviderJar = getCachedGameProviderJar()
+        val mainClass: String = cachedGameProviderJar.inputStream().use { stream ->
+            val manifest: Manifest? = JarInputStream(stream).use { it.manifest }
+            manifest?.mainAttributes?.getValue("Main-Class")
+        } ?: throw IllegalStateException("Main-Class not found in the manifest of the mod jar")
+        this.mainClass.set(mainClass)
+    }
+
+    private fun getCachedGameProviderJar(): File {
+        return LunarCoreRunnerPlugin.gameprovider.singleFile
+    }
+
+    @Override
+    @TaskAction
+    override fun exec() {
+        val lunarCoreFile: File = LunarCoreRunnerPlugin.lunarcore.files.first()
+
+        val gameProviderLibraries: List<File> = LunarCoreRunnerPlugin.gameproviderLibrary.toList()
+
+        val runDir = this.runDir.get()
+        val librariesDir = File(runDir, "libraries")
+        if (!librariesDir.exists()) librariesDir.mkdirs()
+        val modsDir = File(runDir, "mods")
+        if (!modsDir.exists()) modsDir.mkdir()
+
+        val modFile = project.tasks.getByName("jar").outputs.files.singleFile
+
+        val lunarCoreJar = File(runDir, lunarCoreFile.name)
+        lunarCoreFile.copyTo(lunarCoreJar, true)
+        val cachedGameProviderJar = getCachedGameProviderJar()
+        val gameProviderJar = File(runDir, cachedGameProviderJar.name)
+        cachedGameProviderJar.copyTo(gameProviderJar, true)
+
+        val libraryJars: List<File> = gameProviderLibraries.map { library ->
+            val to = File(librariesDir, library.name)
+            library.copyTo(to, true)
+            to
+        }
+
+        this.workingDir = runDir
+        val serverClassPath = project.files(lunarCoreJar, gameProviderJar, *libraryJars.toTypedArray())
+        this.classpath = serverClassPath.plus(project.files(modFile))
+
+        val mainClass: String = gameProviderJar.inputStream().use { stream ->
+            val manifest: Manifest? = JarInputStream(stream).use { it.manifest }
+            manifest?.mainAttributes?.getValue("Main-Class")
+        } ?: throw IllegalStateException("Main-Class not found in the manifest of the mod jar")
+
+        val mixinJar = libraryJars.first { it.name.contains("mixin") }
+        // loads mods from classpath, and debug your mixin with hot-swapping
+        this.jvmArgs("-Dfabric.development=true", "-javaagent:${mixinJar.absolutePath}")
+
+        val cp = serverClassPath.joinToString(";") { runDir.toPath().relativize(it.toPath()).toString() }
+        File(runDir, "args.txt").writeText("-cp $cp\n$mainClass")
+
+        this.standardInput = System.`in`
+        this.isIgnoreExitValue = true
+
+        super.exec()
+    }
+}
