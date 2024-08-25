22c2a65c340ecc6976447b770f040bd4ca6ba7c4
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2024-08-25 14:02:38 +0900

    added publish.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
     kotlin("jvm") version "1.9.23"
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    `java-gradle-plugin`
     `maven-publish`
 }
 
+val ver = "1.0.0"
+
 group = "io.github.lunarcoremods"
-version = "1.0-SNAPSHOT"
+version = System.getenv("COMMIT_SHA") ?: ver
 
 val shade: Configuration = configurations.register("shade") {
     configurations.implementation.get().extendsFrom(this)
@@ -30,14 +31,39 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks.jar {
+tasks.jar.configure {
+    enabled = false
+}
+
+tasks.publish {
     dependsOn(tasks.shadowJar)
 }
 
 tasks.shadowJar {
     configurations = listOf(shade)
+    archiveVersion.set("")
+    archiveClassifier.set("")
 }
 
 kotlin {
     jvmToolchain(17)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/${System.getenv("GITHUB_REPOSITORY")}")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            artifactId = project.name
+            from(components["java"])
+        }
+    }
 }
